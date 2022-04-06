fb9e664e692fb93066dc93d5e182f0236e339172
SD-github21 <sapandonde.gmail.com>
2022-04-05 23:01:25 -0500

    match code to lesson code snapshot before proceeding

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var taskFormHandler = function (event) {
     event.preventDefault();
-
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
     // check if input values are empty strings
-    if (!taskNameInput || !taskTypeInput) {
+    if (taskNameInput === "" || taskTypeInput === "") {
         alert("You need to fill out the task form!");
         return false;
     }
         
     formEl.reset();
+
+      // reset form fields for next task to be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
 
     // package up data as an object
     var taskDataObj = {
@@ -39,10 +43,11 @@ var createTaskEl = function(taskDataObj) {
         taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
         listItemEl.appendChild(taskInfoEl);
     
+        console.dir(listItemEl);
         //add entire list item to list
         tasksToDoEl.appendChild(listItemEl); 
 
-}
+};
 
 formEl.addEventListener("submit", taskFormHandler);
 
